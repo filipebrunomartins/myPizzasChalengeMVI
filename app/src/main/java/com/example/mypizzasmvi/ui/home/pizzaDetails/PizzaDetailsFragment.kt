197ca8f3bc626fc7bd32a80a90f7565ee5d03182
Filipe Martins <filipe2013.martins@gmail.com>
2021-05-19 11:27:24 -0300

    criando tela de detalhes da pizza ainda falta ajuste de troca de tamanho da pizza

--- app/src/main/java/com/example/mypizzasmvi/ui/home/pizzaDetails/PizzaDetailsFragment.kt
+++ app/src/main/java/com/example/mypizzasmvi/ui/home/pizzaDetails/PizzaDetailsFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.mypizzasmvi.R
+import com.example.mypizzasmvi.core.utils.loadImage
+import com.example.mypizzasmvi.core.utils.setOnSingleClickListener
+import com.example.mypizzasmvi.core.utils.toMoneyFormat
+import kotlinx.android.synthetic.main.fragment_details_pizza.*
 
 class PizzaDetailsFragment : Fragment() {
+
+    private val args by navArgs<PizzaDetailsFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +28,20 @@ class PizzaDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupPizzaDetails()
+    }
 
+    private fun setupPizzaDetails() {
+        rattingBarPizza.rating = args.pizzaModel.rating
+        loadImage(args.pizzaModel.imageUrl,imageViewPizzaDetail)
+        textViewNamePizza.text = args.pizzaModel.name
+        textViewValuePizza.text = args.pizzaModel.priceP.toMoneyFormat()
+        buttonSizeP.background = ResourcesCompat.getDrawable(resources, R.drawable.modal_button_size_pizza_selected, null)
+        buttonSizeP.setTextColor(ResourcesCompat.getColor(resources, R.color.white, null))
+
+        textViewBuyPizza.setOnSingleClickListener {
+            Toast.makeText(requireContext(),"teste",Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
